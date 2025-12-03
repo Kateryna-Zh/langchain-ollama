39d25aea0c2b12df66598fdc34750bb484807d36
KaterynaN <kateryna.od.ua@gmail.com>
2025-12-03 19:29:42 +0200

    Notebook with langhain(2025 version)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 
-llm = ChatOllama(model="llama3.2")
+llm = ChatOllama(model="llama3.2", temperature=0.0)
+creative_llm = ChatOllama(model="llama3.2", temperature=0.9)
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful assistant."),
     ("human", "Explain LangChain in simple terms.")
